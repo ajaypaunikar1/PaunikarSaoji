ce17bcd95656e4a55e0ed7c487a7a11c5997b416
unknown <kadurugved0@gmail.com>
2026-07-13 19:59:53 +0530

    added DNS srv fix for server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import dns from 'dns';
+
+// Fallback DNS to resolve MongoDB Atlas Srv records
+dns.setServers(['8.8.8.8', '1.1.1.1']);
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
